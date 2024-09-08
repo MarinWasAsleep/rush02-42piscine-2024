c67628be84f2dc0017a82ec75906ac38960b1110
Ivo Meulemans <imeulema@c1r17s7.42lausanne.ch>
2024-09-08 20:08:05 +0200

    Fonctionne

--- ex00/clean_up/main.c
+++ ex00/clean_up/main.c
@@ -6,31 +6,54 @@
 /*   By: imeulema <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 15:40:12 by imeulema          #+#    #+#             */
-/*   Updated: 2024/09/08 19:05:52 by imeulema         ###   ########.fr       */
+/*   Updated: 2024/09/08 19:58:37 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
-	int	i;
-	char *number;
-	char *dict_str;
-	char **tab;
+	int		i;
+	char	*number;
+	char	*dict_str;
+	char	*path;
+	char	**tab;
+	t_dict	*dict;
 
-	if (ac != 2 || ac != 3)
+	if (ac != 2 && ac != 3)
 	{
-		write(1, "Error\n", 6);
+		write(1, "Erro0\n", 6);
 		return (1);
 	}
 	else if (ac == 2)
+	{
+		path = "numbers.dict";
 		number = av[1];
+	}
 	else if (ac == 3)
+	{
+		path = av[1];
 		number = av[2];
-	tab = get_number(str)
-	dict_str = ft_parse_dict() // dict path?
-	split_dict()
-	write_number()
-	free_mem()
+	}
+	printf("Ready to get number\n");
+	tab = get_number(number);
+	printf("Number retrieved\n");
+	if (tab == NULL)
+	{
+		write(1, "Erro1\n", 6);
+		return (1);
+	}
+	printf("Ready to parse dict\n");
+	dict_str = ft_parse_dict(path);
+	printf("Dict parsed\n");
+	if (dict_str == NULL)
+		return (1);
+	printf("Ready to split dict\n");
+	dict = split_dict(dict_str);
+	printf("Dict split\n");
+	//write_number()
+	//free_mem()
+	return (0);
 }
